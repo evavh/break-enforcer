f5ea43b860773f31f348cf8020d47673cd36a1fd
Eva van Houten <opensource@evavh.net>
2023-02-06 11:46:29 +0100

    Change led to simple on/off, organise code

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,21 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::{
-    gpio::{Level, Output, Pin, Speed},
-    pwm::{
-        simple_pwm::{PwmPin, SimplePwm},
-        Channel, Channel1Pin,
-    },
-    time::hz,
-};
+use embassy_stm32::gpio::{Level, Output, Pin, Speed};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
-const WORK_MINUTES: u64 = 60;
-const WARNING_PERIOD_SECS: u64 = 10;
-const BREAK_MINUTES: u64 = 10;
+const WORK: Duration = Duration::from_secs(20);
+const NOTICE_PERIOD: Duration = Duration::from_secs(10);
+const WARNING_PERIOD: Duration = Duration::from_secs(5);
+const BREAK: Duration = Duration::from_secs(10);
 
-const BEEP_DURATION_MILLIS: u64 = 500;
+const SHORT_BEEP: Duration = Duration::from_millis(200);
+const LONG_BEEP: Duration = Duration::from_millis(600);
 
-async fn beep<T: Pin>(speaker: &mut Output<'_, T>) {
+async fn beep<T: Pin>(speaker: &mut Output<'_, T>, duration: Duration) {
     speaker.set_high();
-    Timer::after(Duration::from_millis(BEEP_DURATION_MILLIS)).await;
+    Timer::after(duration).await;
     speaker.set_low();
 }
 
@@ -33,56 +28,50 @@ async fn main(_spawner: Spawner) {
     info!("Hello World!");
 
     let mut usb_power = Output::new(p.PC13, Level::High, Speed::Low);
-    //    let mut speaker = Output::new(p.PA6, Level::High, Speed::Low);
-    let green_pin = PwmPin::new_ch2(p.PA1);
-    let red_pin = PwmPin::new_ch3(p.PA2);
-    let blue_pin = PwmPin::new_ch4(p.PA3);
+    let mut speaker = Output::new(p.PA6, Level::High, Speed::Low);
+    let mut red = Output::new(p.PA2, Level::High, Speed::Low);
+    let mut green = Output::new(p.PA1, Level::High, Speed::Low);
+    let mut blue = Output::new(p.PA3, Level::High, Speed::Low);
 
-    let mut led = SimplePwm::new(
-        p.TIM2,
-        None,
-        Some(green_pin),
-        Some(red_pin),
-        Some(blue_pin),
-        hz(2000),
-    );
-
-    // Timer doesnt work anymore when using pwm led
-    let max_duty = led.get_max_duty();
-    led.set_duty(Channel::Ch2, max_duty / 2);
-    led.set_duty(Channel::Ch3, max_duty / 2);
-    led.set_duty(Channel::Ch4, max_duty / 2);
-
-    led.enable(Channel::Ch2);
-    led.enable(Channel::Ch3);
-    led.enable(Channel::Ch4);
+    speaker.set_low();
 
     loop {
-        info!("high: work");
+        info!("work for {}m", WORK.as_secs() / 60);
+        beep(&mut speaker, SHORT_BEEP).await;
+        //green led
+        red.set_low();
+        green.set_high();
+        blue.set_low();
+
         usb_power.set_high();
-        println!("Groen aan");
-        //Timer::after(Duration::from_secs(2)).await;
-        println!("Groen uit");
-        led.disable(Channel::Ch2);
-        //Timer::after(Duration::from_secs(2)).await;
-        led.disable(Channel::Ch3);
-        //Timer::after(Duration::from_secs(2)).await;
-        led.disable(Channel::Ch4);
-        info!("waiting for {} minutes", WORK_MINUTES);
-        //Timer::after(Duration::from_secs(WORK_MINUTES * 60 - WARNING_PERIOD_SECS)).await;
-        Timer::after(Duration::from_secs(15)).await;
+        Timer::after(WORK - NOTICE_PERIOD).await;
 
-        info!(
-            "sending warning {} seconds before break",
-            WARNING_PERIOD_SECS
-        );
-        //beep(&mut speaker);
-        //Timer::after(Duration::from_millis(WARNING_PERIOD_SECS * 1000 - BEEP_DURATION_MILLIS)).await;
-        Timer::after(Duration::from_millis(0)).await;
+        info!("sending notice {}s before break", NOTICE_PERIOD.as_secs());
+        beep(&mut speaker, SHORT_BEEP).await;
+        //blue led
+        red.set_low();
+        green.set_low();
+        blue.set_high();
 
-        info!("low: break");
+        Timer::after(NOTICE_PERIOD - SHORT_BEEP - WARNING_PERIOD).await;
+
+        info!("sending warning {}s before break", WARNING_PERIOD.as_secs());
+        beep(&mut speaker, LONG_BEEP).await;
+        //orangeish led
+        red.set_high();
+        green.set_high();
+        blue.set_low();
+
+        Timer::after(WARNING_PERIOD - LONG_BEEP).await;
+
+        info!("break for {}m", BREAK.as_secs() / 60);
+        beep(&mut speaker, SHORT_BEEP).await;
+        //red led
+        red.set_high();
+        green.set_low();
+        blue.set_low();
+
         usb_power.set_low();
-        //Timer::after(Duration::from_secs(BREAK_MINUTES * 60)).await;
-        Timer::after(Duration::from_secs(10)).await;
+        Timer::after(BREAK).await;
     }
 }
