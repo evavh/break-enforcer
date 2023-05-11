8a1c8e12291eb2e111a35db50fdcb219c95aa01d
dvdsk <noreply@davidsk.dev>
2023-05-11 11:50:35 +0200

    put interrupt code in RAM

--- spy/src/main.rs
+++ spy/src/main.rs
@@ -3,17 +3,18 @@
 
 use defmt::info;
 use defmt_rtt as _;
-use hal::gpio::Pin;
+use hal::{
+    gpio::{self, Pin},
+    pac::Peripherals,
+    prelude::_stm32f4xx_hal_gpio_GpioExt,
+    syscfg::SysCfgExt,
+};
 // global logger
 use panic_probe as _;
 
 use stm32f4xx_hal as hal; // includes memory.x?
 
-use crate::hal::{
-    gpio,
-    pac::{interrupt, Peripherals},
-    prelude::*,
-};
+use crate::hal::{pac::interrupt, prelude::_stm32f4xx_hal_gpio_ExtiPin};
 
 use core::{
     cell::Cell,
@@ -55,6 +56,9 @@ unsafe fn force_mut<T>(reference: &T) -> &mut T {
 //
 // The IRQ uses polling to get the data faster. After
 // the IRQ the data will be analyzed
+
+// put interrupt code in ram (.data is kept in ram)
+#[link_section = ".data.EXTI1"] 
 #[interrupt]
 fn EXTI1() {
     cortex_m::interrupt::free(|cs| {
@@ -64,7 +68,7 @@ fn EXTI1() {
                 .get_mut()
                 .as_mut()
                 .unwrap_unchecked()
-                // .expect("pin is set before ISR is enabled")
+            // .expect("pin is set before ISR is enabled")
         };
         usb.clear_interrupt_pending_bit();
         for m in array {
@@ -76,44 +80,44 @@ fn EXTI1() {
 
 #[entry]
 fn main() -> ! {
-    // let mut dp = Peripherals::take().unwrap();
-    //
-    // // set power on usb (prototype needs this)
-    // let gpio_c = dp.GPIOC.split();
-    // let mut usb_enable = gpio_c.pc13.into_push_pull_output();
-    // usb_enable.set_high();
-    //
-    // let gpio_b = dp.GPIOB.split();
-    // let mut usb = gpio_b.pb1.into_floating_input();
-    //
-    // let mut syscfg = dp.SYSCFG.constrain();
-    // usb.make_interrupt_source(&mut syscfg);
-    // usb.enable_interrupt(&mut dp.EXTI);
-    // usb.trigger_on_edge(&mut dp.EXTI, gpio::Edge::Falling);
-    // let interrupt_number = usb.interrupt();
-    //
-    // cortex_m::interrupt::free(|cs| USB.borrow(cs).swap(&Cell::new(Some(usb))));
-    //
-    // // info!("{:?}", interrupt_number);
-    // // clear pending interrupts on usb gpio
-    // cortex_m::peripheral::NVIC::unpend(interrupt_number);
-    // unsafe {
-    //     // enable interrupt on usb gpio
-    //     cortex_m::peripheral::NVIC::unmask(interrupt_number);
-    // }
-    //
-    // loop {
-    //     while !DONE
-    //         .compare_exchange(true, false, Ordering::Relaxed, Ordering::Relaxed)
-    //         .is_ok()
-    //     {}
-    //     let mut array = [false; 80];
-    //     cortex_m::interrupt::free(|cs| {
-    //         let shared = ARRAY.borrow(cs);
-    //         array = shared.clone();
-    //     });
-    //
-    //     info!("array: {}", array);
-    // }
+    let mut dp = Peripherals::take().unwrap();
+
+    // set power on usb (prototype needs this)
+    let gpio_c = dp.GPIOC.split();
+    let mut usb_enable = gpio_c.pc13.into_push_pull_output();
+    usb_enable.set_high();
+
+    let gpio_b = dp.GPIOB.split();
+    let mut usb = gpio_b.pb1.into_floating_input();
+
+    let mut syscfg = dp.SYSCFG.constrain();
+    usb.make_interrupt_source(&mut syscfg);
+    usb.enable_interrupt(&mut dp.EXTI);
+    usb.trigger_on_edge(&mut dp.EXTI, gpio::Edge::Falling);
+    let interrupt_number = usb.interrupt();
+
+    cortex_m::interrupt::free(|cs| USB.borrow(cs).swap(&Cell::new(Some(usb))));
+
+    // info!("{:?}", interrupt_number);
+    // clear pending interrupts on usb gpio
+    cortex_m::peripheral::NVIC::unpend(interrupt_number);
+    unsafe {
+        // enable interrupt on usb gpio
+        cortex_m::peripheral::NVIC::unmask(interrupt_number);
+    }
+
+    loop {
+        while !DONE
+            .compare_exchange(true, false, Ordering::Relaxed, Ordering::Relaxed)
+            .is_ok()
+        {}
+        let mut array = [false; 80];
+        cortex_m::interrupt::free(|cs| {
+            let shared = ARRAY.borrow(cs);
+            array = shared.clone();
+        });
+
+        info!("array: {}", array);
+    }
     exit()
 }
