d9dcaa18e4153198f5401f8ff05e5a61513e9212
dvdsk <noreply@davidsk.dev>
2023-05-13 14:00:18 +0200

    fixes passing statics to asm macro

--- spy/build.rs
+++ spy/build.rs
@@ -1,4 +1,13 @@
+// use std::{env, error::Error, fs::File, io::Write, path::PathBuf};
+//
+// use cc::Build;
+
 fn main() {
+    // // build directory for this crate
+    // let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    //
+    // println!("cargo:rustc-link-search={}", out_dir.display());
+
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
--- spy/src/read_packets.rs
+++ spy/src/read_packets.rs
@@ -1,8 +1,4 @@
-use crate::hal::pac::interrupt;
-
-use core::{arch::asm, sync::atomic::Ordering};
-use stm32f4xx_hal as hal;
-
+use core::arch::global_asm;
 use crate::{ARRAY, DONE};
 
 // stm32f4 has minimum 12 cycle interrupt delay
@@ -18,58 +14,54 @@ use crate::{ARRAY, DONE};
 //
 // https://developer.arm.com/documentation/100166/0001/Programmers-Model/Instruction-set-summary/Table-of-processor-instructions
 //
-#[link_section = ".data.EXTI1"]
-#[interrupt]
-fn EXTI1() {
-    unsafe {
-        // let idr = &(*hal::pac::GPIOB::ptr()).idr;
-        // *ARRAY.get_unchecked_mut(1) = idr.read().bits();
-        asm!(
-            // Prepare registers
-            //
-            // pin adress in r0
-            "movw r0, #1040",                             // 1 cycle
-            // keep start of ARRAY in r2
-            "movw r2, :lower16:break_spy::ARRAY",         // 1 cycle
-            // continue moving pin addr to r0
-            "movt r0, #16386",                            // 1 cycle
-            // continue putting ARRAY in r2
-            "movt r2, :upper16:break_spy::ARRAY",        // 1 cycle
+
+global_asm! {
+    ".section .data.EXTI1",
+    ".global EXTI1",
+    ".syntax unified",
+    ".thumb_func",
+"EXTI1:",
+    // Prepare registers
+    //
+    // pin adress in r0
+    "movw r0, #1040",                             // 1 cycle
+    // keep start of ARRAY in r2
+    "movw r2, :lower16:{ARRAY}",         // 1 cycle
+    // continue moving pin addr to r0
+    "movt r0, #16386",                            // 1 cycle
+    // continue putting ARRAY in r2
+    "movt r2, :upper16:{ARRAY}",         // 1 cycle
 
 
-            // Store gpio value in ARRAY
-            //
-            // load the current value of the pin into r1
-            "ldr r1, [r0]",                              // 2 cycles
-            // store r1 in the adress pointed to by r2 with 4 added
-            "str r1, [r2, #4]",                          // 2 cycles
-            "NOP",                                       // 1 cycle
-            "NOP",                                       // 1 cycle
-            "NOP",                                       // 1 cycle
-            // = 7 cycles
+    // Store gpio value in ARRAY
+    //
+    // load the current value of the pin into r1
+    "ldr r1, [r0]",                               // 2 cycles
+    // store r1 in the adress pointed to by r2 with 4 added
+    "str r1, [r2, #4]",                          // 2 cycles
+    "NOP",                                       // 1 cycle
+    "NOP",                                       // 1 cycle
+    "NOP",                                       // 1 cycle
+    // = 7 cycles
 
-            // Set interrupt as handled
-            //
-            // set memory adress of interrupt pending in r0
-            "movw r0, #15380",                           // 1 cycle
-            "movt r0, #16385",                           // 1 cycle
-            // set interrupt pending to 2/confirm handled 
-            "movs r1, #2",                               // 1 cycle
-            "str r1, [r0]",                              // 2 cycles
+    // Set interrupt as handled
+    //
+    // set memory adress of interrupt pending in r0
+    "movw r0, #15380",                           // 1 cycle
+    "movt r0, #16385",                           // 1 cycle
+    // set interrupt pending to 2/confirm handled
+    "movs r1, #2",                               // 1 cycle
+    "str r1, [r0]",                              // 2 cycles
 
-            // Mark as done for non interrupt code
-            //
-            // set r0 to break_spy the adress of DONE
-            "movw r0, :lower16:break_spy::DONE",         // 1 cycle
-            "movt r0, :upper16:break_spy::DONE",         // 1 cycle
-            // set r1 to the pr1 bit (1)
-            "mov r1, #1",                                // 1 cycle
-            // set DONE to r1 (which is 1)
-            "strb r1, [r0]",                             // 2 cycles
-        );
-        // set interrupt pending to false/confirm interrupt handled
-        // (*hal::pac::EXTI::ptr()).pr.write(|w| w.pr1().set_bit());
-    }
-    // signal packet rdy to code after interrupt
-    // DONE.store(true, Ordering::Relaxed);
+    // Mark as done for non interrupt code
+    //
+    // set r0 to break_spy the adress of DONE
+    "movw r0, :lower16:{DONE}",         // 1 cycle
+    "movt r0, :upper16:{DONE}",         // 1 cycle
+    // set r1 to the pr1 bit (1)
+    "mov r1, #1",                                // 1 cycle
+    // set DONE to r1 (which is 1)
+    "strb r1, [r0]",                             // 2 cycles
+    ARRAY = sym ARRAY,
+    DONE = sym DONE,
 }
