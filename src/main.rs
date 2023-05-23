f835d9b0143156678f2e9e8d3667883d8abbce25
dvdsk <noreply@davidsk.dev>
2023-05-23 02:20:42 +0200

    PLL now runs at ~5*16Mhz :)

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,9 @@ use cortex_m_rt::entry;
 // based on: https://controllerstech.com/stm32-clock-setup-using-registers/
 // https://kleinembedded.com/stm32-without-cubeide-part-2-cmsis-make-and-clock-configuration/
 
+// c hal sources:
+// https://github.com/zephyrproject-rtos/hal_stm32/blob/c865374fc83d93416c0f380e6310368ff55d6ce2/stm32cube/stm32f0xx/drivers/include/stm32f0xx_hal_rcc.h#L751
+
 #[entry]
 fn main() -> ! {
     // Setup handler for device peripherals
@@ -33,86 +36,112 @@ fn main() -> ! {
     dp.RCC.cr.write(|w| w.hseon().set_bit());
 
     // Wait for HSE clock to become ready
-    while dp.RCC.cr.read().hserdy().is_ready() {}
+    while dp.RCC.cr.read().hserdy().is_not_ready() {}
 
-    // Set the power enable clock and voltage regulator
+    // dp.PWR.cr.write(|w| w.dbp().set_bit());
+    // delay(5);
+    // dp.RCC.bdcr.write(|w| w.bdrst().enabled());
+    // delay(5);
+    // // Enable LSE clock
+    // dp.RCC.bdcr.write(|w| w.lseon().on());
+    // info!("Wait for LSE clock to become ready");
+    // while dp.RCC.bdcr.read().lserdy().is_not_ready() {}
+
+    // Set the power interface clock to on
     dp.RCC.apb1enr.write(|w| w.pwren().enabled());
     delay(10);
-    // Regulator voltage scaling output selection,
-    // needed for high clock speed
-    // need to turn pll off first
-    dp.RCC.cr.write(|w| w.pllon().off());
-    // wait for pll to be off
 
-    info!("waiting for pll to turn off");
-    while !dp.RCC.cr.read().pllrdy().is_not_ready() {}
+    // enable instruction cache, prefetch buffer
+    // data cache and set flash latency to 5 wait states
+    dp.FLASH.acr.write(|w| {
+        w.icen().set_bit();
+        w.prften().set_bit();
+        w.dcen().set_bit();
+        w.latency().ws5()
+    });
+
+    // Configure bus prescalars
+    unsafe {
+        dp.RCC.cfgr.write(|w| {
+            w.hpre().div1(); // AHB prescalar
+            w.ppre1().bits(2);
+            w.ppre2().bits(1)
+        })
+    }
+
+    const PLLM: u8 = 25;
+    const PLLN: u16 = 168;
+    const PLLP_DIV_2: u8 = 0;
+    unsafe {
+        dp.RCC.pllcfgr.write(|w| {
+            w.pllsrc().hse();
+            w.pllm().bits(PLLM);
+            w.plln().bits(PLLN);
+            w.pllp().bits(PLLP_DIV_2)
+        });
+    }
+
+    // turn on pll
+    dp.RCC.cr.write(|w| {
+        w.hseon().on();
+        w.pllon().on()
+    });
 
     const SCALE_TWO: u8 = 0b10;
     unsafe {
         dp.PWR.cr.write(|w| w.vos().bits(SCALE_TWO));
     }
-
-    // wait for VOS to become ready
     info!("waiting for voltage output selection to be ready");
-    while !dp.PWR.csr.read().vosrdy().bit_is_set() {}
-
-    // enable instruction cache, prefetch buffer
-    // data cache and set flash latency to 5 wait states
-    dp.FLASH.acr.write(|w| w.icen().set_bit());
-    dp.FLASH.acr.write(|w| w.prften().set_bit());
-    dp.FLASH.acr.write(|w| w.dcen().set_bit());
-    dp.FLASH.acr.write(|w| w.latency().ws5());
-
-    // Configure bus prescalars
-    dp.RCC.cfgr.write(|w| w.hpre().div1()); // AHB prescalar
-    dp.RCC.cfgr.write(|w| unsafe { w.ppre1().bits(2) });
-    dp.RCC.cfgr.write(|w| unsafe { w.ppre2().bits(1) });
-
-    unsafe {
-        dp.RCC.pllcfgr.write(|w| w.pllsrc().hse());
-
-        const PLLM: u8 = 25;
-        dp.RCC.pllcfgr.write(|w| w.pllm().bits(PLLM));
-
-        const PLLN: u16 = 168;
-        dp.RCC.pllcfgr.write(|w| w.plln().bits(PLLN));
-
-        const PLLP: u8 = 2;
-        dp.RCC.pllcfgr.write(|w| w.pllp().bits(PLLP));
-    }
-
-    dp.RCC.cr.write(|w| w.pllon().off());
+    while dp.PWR.csr.read().vosrdy().bit_is_clear() {}
 
     info!("waiting for PLL to become ready");
-    while !dp.RCC.cr.read().pllrdy().bit_is_set() {}
+    while dp.RCC.cr.read().pllrdy().is_not_ready() {}
 
     // Select PLL as System Clock Source
-    dp.RCC.cfgr.write(|w| w.sw().pll());
+    unsafe {
+        dp.RCC.cfgr.write(|w| {
+            w.sw().pll();
+            w.hpre().div1(); // AHB prescalar
+            w.ppre1().bits(2);
+            w.ppre2().bits(1)
+        });
+    }
 
     info!("waiting for PLL to be selected as System Clock Source");
     while !dp.RCC.cfgr.read().sws().is_pll() {}
 
-    // pll div5 measures 3.2Mhz (* 5 = 16 Mhz)
-    // hse div5 also measures 3.2Mhz (* 5 = 16 Mhz)
-
-    // enable MCO1 HSE output
-    dp.RCC.cfgr.write(|w| w.mco1().pll());
-    dp.RCC.cfgr.write(|w| w.mco1pre().div5());
-
-    //Enable Clock to GPIOA
+    //Enable Clock to GPIOA for mco1 and debug pin
     dp.RCC.ahb1enr.write(|w| w.gpioaen().set_bit());
 
-    //Configure PA5 as Output
-    dp.GPIOA.moder.write(|w| w.moder8().output());
-    // dp.GPIOA.otyper.write(|w| w.ot8().push_pull());
-
-    // // Set PA5 Output to High signalling end of configuration
-    // dp.GPIOA.odr.write(|w| w.odr8().low());
-
-    dp.GPIOA.ospeedr.write(|w| w.ospeedr8().very_high_speed());
-    dp.GPIOA.moder.write(|w| w.moder8().alternate());
+    dp.GPIOA.moder.write(|w| {
+        w.moder0().output();
+        w.moder8().alternate()
+    });
     dp.GPIOA.afrh.write(|w| w.afrh8().af0());
+
+    // enable MCO1 HSE output
+    // pll div5 measures 3.2Mhz (* 5 = 16 Mhz)
+    // hse div5 also measures 3.2Mhz (* 5 = 16 Mhz)
+    unsafe {
+        dp.RCC.cfgr.write(|w| {
+            w.mco1pre().div5();
+            w.mco1().pll();
+            w.sw().pll();
+            w.hpre().div1(); // AHB prescalar
+            w.ppre1().bits(2);
+            w.ppre2().bits(1)
+        });
+    }
+
     info!("ready");
 
-    loop {}
+    //Configure PA as Output
+    dp.GPIOA.otyper.write(|w| w.ot0().push_pull());
+    dp.GPIOA.ospeedr.write(|w| w.ospeedr0().very_high_speed());
+
+    loop {
+        dp.GPIOA.odr.write(|w| w.odr0().low());
+        delay(1000);
+        dp.GPIOA.odr.write(|w| w.odr0().high());
+    }
 }
