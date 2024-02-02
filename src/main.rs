358ac32b6835d2474168ae72f19b71ee0dcf2e7b
Eva van Houten <opensource@evavh.net>
2024-02-02 13:40:18 +0100

    Add notify function with sudo hack

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    process::Command,
     sync::{
         atomic::{AtomicBool, Ordering},
         mpsc::{channel, RecvTimeoutError},
@@ -8,8 +9,13 @@ use std::{
     time::Duration,
 };
 
-use self::check_inputs::inactivity_watcher;
+mod check_inputs;
+
+use crate::check_inputs::inactivity_watcher;
 use crate::check_inputs::wait_for_any_input;
+
+const USER: &str = "focus";
+const UID: &str = "1004";
 
 const MOUSE_DEVICE: &str = "/dev/input/mice";
 const KEYBOARD_DEVICE: &str = "/dev/input/by-id/usb-046a_010d-event-kbd";
@@ -17,8 +23,6 @@ const ALL_DEVICES: [&str; 2] = [MOUSE_DEVICE, KEYBOARD_DEVICE];
 
 const T_BREAK: Duration = Duration::from_secs(5);
 const T_WORK: Duration = Duration::from_secs(15);
-
-mod check_inputs;
 
 fn main() {
     let (break_skip_sender, break_skip_receiver) = channel();
@@ -39,25 +43,30 @@ fn main() {
     }
 
     loop {
-        println!("Keyboard on!");
-        println!("Waiting for input to start work timer...");
+        notify("Keyboard on!");
+        notify("Waiting for input to start work timer...");
         wait_for_any_input(ALL_DEVICES);
-        println!("Starting work timer for {T_WORK:?}");
+        notify("Starting work timer for {T_WORK:?}");
         work_start_sender.send(true).unwrap();
         match break_skip_receiver.recv_timeout(T_WORK) {
             Ok(_) => {
-                println!("No input for breaktime, skip break");
-                println!("Waiting for input to restart work timer...");
+                notify("No input for breaktime, skip break");
+                notify("Waiting for input to restart work timer...");
                 wait_for_any_input(ALL_DEVICES);
-                println!("\nRestarting work");
+                notify("Restarting work");
                 break_skip_is_sent.store(false, Ordering::Release);
                 continue;
             }
             Err(RecvTimeoutError::Timeout) => (),
             Err(e) => panic!("Unexpected error: {e}"),
         }
-        println!("\nKeyboard off!");
-        println!("Starting break timer for {T_BREAK:?}\n");
+        notify("Keyboard off!");
+        notify("Starting break timer for {T_BREAK:?}");
         thread::sleep(T_BREAK);
     }
+}
+
+fn notify(text: &str) {
+    let command = format!("sudo -u {USER} DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/{UID}/bus notify-send -t 5000 \"{text}\"");
+    Command::new("sh").arg("-c").arg(command).output().unwrap();
 }
