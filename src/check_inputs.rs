d9310fbfd7a5742b7a266c8d2cbfd4e9bb29e787
Eva van Houten <opensource@evavh.net>
2024-02-07 14:27:43 +0100

    Working on fixing infinite files

--- src/check_inputs.rs
+++ src/check_inputs.rs
@@ -13,24 +13,22 @@ use std::{
 use crate::T_BREAK;
 
 // TODO get file open out of here, runs too often
-pub fn wait_for_input(device: &str) -> Instant {
-    let mut file = File::open(device).unwrap();
+pub fn wait_for_input(file: &mut File) -> Instant {
     let mut packet = [0u8; 24];
     file.read_exact(&mut packet).unwrap();
 
     Instant::now()
 }
 
-pub fn wait_for_any_input(devices: [&'static str; 2]) -> Instant {
+pub fn wait_for_any_input(files: &mut [File; 2]) -> Instant {
     let (send, recv) = channel();
 
-    for device in devices {
+    for file in files {
         let send = send.clone();
 
         thread::Builder::new()
-            .name(device.to_string())
             .spawn(move || {
-                wait_for_input(device);
+                wait_for_input(&mut file);
                 let _ = send.send(true);
             })
             .unwrap();
@@ -46,11 +44,13 @@ pub fn inactivity_watcher(
     break_skip_sender: &Sender<bool>,
     break_skip_sent: &Arc<AtomicBool>,
 ) {
+    let mut files = devices.map(File::open).map(Result::unwrap);
+
     work_start_receiver.recv().unwrap();
 
     let (input_sender, input_receiver) = channel();
     thread::spawn(move || loop {
-        wait_for_any_input(devices);
+        wait_for_any_input(&mut files);
         input_sender.send(true).unwrap();
     });
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs::File,
     process::Command,
     sync::{
         atomic::{AtomicBool, Ordering},
@@ -30,6 +31,9 @@ const T_WORK: Duration = Duration::from_secs(15 * 60);
 
 fn main() {
     dbg!(list_devices());
+
+    let mut device_files = ALL_DEVICES.map(File::open).map(Result::unwrap);
+
     // let keyboard_dev = Device {
     //     event_path: KEYBOARD_EVENT.to_string(),
     //     name: "HID 046a:010d".to_string(),
@@ -39,50 +43,60 @@ fn main() {
     //     name: "HSMshift".to_string(),
     // };
 
-    // let (break_skip_sender, break_skip_receiver) = channel();
-    // let (work_start_sender, work_start_receiver) = channel();
-    // let break_skip_is_sent = Arc::new(AtomicBool::new(false));
-    //
-    // {
-    //     let break_skip_is_sent = break_skip_is_sent.clone();
-    //
-    //     thread::spawn(move || {
-    //         inactivity_watcher(
-    //             ALL_DEVICES,
-    //             &work_start_receiver,
-    //             &break_skip_sender,
-    //             &break_skip_is_sent,
-    //         );
-    //     });
-    // }
-    //
-    // loop {
-    //     notify_all_users("Keyboard on!");
-    //     notify_all_users("Waiting for input to start work timer...");
-    //     wait_for_any_input(ALL_DEVICES);
-    //     notify_all_users(&format!("Starting work timer for {T_WORK:?}"));
-    //     work_start_sender.send(true).unwrap();
-    //     match break_skip_receiver.recv_timeout(T_WORK) {
-    //         Ok(_) => {
-    //             notify_all_users("No input for breaktime, skip break");
-    //             notify_all_users("Waiting for input to restart work timer...");
-    //             wait_for_any_input(ALL_DEVICES);
-    //             notify_all_users("Restarting work");
-    //             break_skip_is_sent.store(false, Ordering::Release);
-    //             continue;
-    //         }
-    //         Err(RecvTimeoutError::Timeout) => (),
-    //         Err(e) => panic!("Unexpected error: {e}"),
-    //     }
-    //     {
-    //         let _mouse = mouse_dev.clone().lock();
-    //         let _keyboard = keyboard_dev.clone().lock();
-    //
-    //         notify_all_users("Keyboard off!");
-    //         notify_all_users(&format!("Starting break timer for {T_BREAK:?}"));
-    //         thread::sleep(T_BREAK);
-    //     }
-    // }
+    let (break_skip_sender, break_skip_receiver) = channel();
+    let (work_start_sender, work_start_receiver) = channel();
+    let break_skip_is_sent = Arc::new(AtomicBool::new(false));
+
+    {
+        let break_skip_is_sent = break_skip_is_sent.clone();
+
+        thread::spawn(move || {
+            inactivity_watcher(
+                ALL_DEVICES,
+                &work_start_receiver,
+                &break_skip_sender,
+                &break_skip_is_sent,
+            );
+        });
+    }
+
+    loop {
+        notify_all_users("Keyboard on!");
+        notify_all_users("Waiting for input to start work timer...");
+        wait_for_any_input(&mut device_files);
+        notify_all_users(&format!("Starting work timer for {T_WORK:?}"));
+        work_start_sender.send(true).unwrap();
+        match break_skip_receiver.recv_timeout(T_WORK) {
+            Ok(_) => {
+                notify_all_users("No input for breaktime, skip break");
+                notify_all_users("Waiting for input to restart work timer...");
+                wait_for_any_input(&mut device_files);
+                notify_all_users("Restarting work");
+                break_skip_is_sent.store(false, Ordering::Release);
+                continue;
+            }
+            Err(RecvTimeoutError::Timeout) => (),
+            Err(e) => panic!("Unexpected error: {e}"),
+        }
+        {
+            for mouse in MOUSE_NAMES.map(find_event).into_iter().flatten() {
+                mouse.clone().lock();
+            }
+            for keyboard in find_event(KEYBOARD_NAME) {
+                keyboard.clone().lock();
+            }
+
+            notify_all_users("Keyboard off!");
+            notify_all_users(&format!("Starting break timer for {T_BREAK:?}"));
+            thread::sleep(T_BREAK);
+        }
+    }
+}
+
+fn find_event(name: &str) -> Vec<Device> {
+    let devices = lock::list_devices();
+    
+    devices.into_iter().filter(|x| x.name == name).collect()
 }
 
 fn notify(username: &str, uid: &str, text: &str) {
