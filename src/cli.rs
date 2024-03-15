989a40a4f5459fed8632345c8216675480050d68
dvdsk <noreply@davidsk.dev>
2024-03-15 11:54:32 +0100

    adds cli and find_devices/list option

---
+++ src/cli.rs
@@ -0,0 +1,101 @@
+use clap::{Parser, Subcommand};
+use std::num::ParseFloatError;
+use std::path::PathBuf;
+use std::time::Duration;
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    Run {
+        /// Period after which input will be disabled.
+        /// run help for the format.
+        #[arg(short, long, value_name = "work", value_parser = parse_duration)]
+        work_duration: Duration,
+        /// Length of the breaks, after this period input is resumed.
+        /// run help for the format.
+        #[arg(short, long, value_name = "break", value_parser = parse_duration)]
+        break_duration: Duration,
+        /// Duration ahead of the break to show a notification
+        /// run help for the format.
+        #[arg(short, long, value_name = "warn", value_parser = parse_duration)]
+        grace_duration: Duration,
+    },
+    FindDevices,
+}
+
+/// Disables specified input devices during breaks. The period between breaks,
+/// length of the breaks and time before getting a warning can all be specified.
+///
+/// Durations can be passed in two formats:
+///  - <amount><unit>, for example: 32m
+///    unit is one of h,m and s
+///  - hh:mm:ss, where hh and mm are optional
+///    example: 1:30:15
+///         one and a halve hour and 15 seconds
+///    example: 10:40
+///         ten minutes and 40 seconds
+///
+#[derive(Parser)]
+#[command(version, about, verbatim_doc_comment)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+    /// Path to create/read/update list of devices to/from
+    #[arg(short, long)]
+    pub config_path: Option<PathBuf>,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ParseError {
+    #[error("Could not parse the second part of the time as number")]
+    Second(ParseFloatError, String),
+    #[error("Could not parse the minute part of the time as number")]
+    Minute(ParseFloatError, String),
+    #[error("Could not parse the minute part of the time as number")]
+    Hour(ParseFloatError, String),
+}
+
+macro_rules! err_builder {
+    ($name:ident, $variant:expr) => {
+        fn $name(e: ParseFloatError, s: &str) -> ParseError {
+            $variant(e, s.to_owned())
+        }
+    };
+}
+
+err_builder!(second_err, ParseError::Second);
+err_builder!(minute_err, ParseError::Minute);
+err_builder!(hour_err, ParseError::Hour);
+
+/// Parses a string in format
+///     hh:mm:ss,
+///     mm:ss,
+///     ss,
+pub(crate) fn parse_colon_duration(arg: &str) -> Result<f32, ParseError> {
+    let Some((rest, seconds)) = arg.rsplit_once(':') else {
+        return arg.parse().map_err(|e| second_err(e, arg));
+    };
+    let mut seconds = seconds.parse().map_err(|e| second_err(e, arg))?;
+    let Some((hours, minutes)) = rest.rsplit_once(':') else {
+        return Ok(seconds);
+    };
+    seconds += 60.0 * minutes.parse::<f32>().map_err(|e| minute_err(e, minutes))?;
+    if hours.is_empty() {
+        return Ok(seconds);
+    };
+    seconds += 60.0 * 60.0 * hours.parse::<f32>().map_err(|e| hour_err(e, hours))?;
+    Ok(seconds)
+}
+
+/// Parse a string in format hh:mm:ss to a `Duration`
+pub(crate) fn parse_duration(arg: &str) -> Result<Duration, ParseError> {
+    let seconds = if let Some(hours) = arg.strip_suffix('h') {
+        60. * 60. * hours.parse::<f32>().map_err(|e| hour_err(e, hours))?
+    } else if let Some(minutes) = arg.strip_suffix('m') {
+        60. * minutes.parse::<f32>().map_err(|e| minute_err(e, minutes))?
+    } else if let Some(seconds) = arg.strip_suffix('s') {
+        seconds.parse::<f32>().map_err(|e| second_err(e, seconds))?
+    } else {
+        parse_colon_duration(arg)?
+    };
+    Ok(std::time::Duration::from_secs_f32(seconds))
+}
---
+++ src/config.rs
@@ -0,0 +1,46 @@
+use color_eyre::eyre::{eyre, Context};
+use color_eyre::{Result, Section};
+
+use std::fs;
+use std::io::ErrorKind;
+use std::path::{Path, PathBuf};
+
+use crate::Device;
+
+fn setup_default_path() -> PathBuf {
+    let dir = Path::new(concat!("/etc/", env!("CARGO_CRATE_NAME"), ".toml"));
+    assert!(dir.is_dir(), "/etc should exist on unix");
+    dir.to_path_buf()
+}
+
+pub(crate) fn read(custom_path: Option<PathBuf>) -> Result<Vec<Device>> {
+    let path = custom_path.unwrap_or_else(setup_default_path);
+    let bytes = match fs::read(&path) {
+        Ok(bytes) => bytes,
+        Err(err) if err.kind() == ErrorKind::NotFound => return Ok(Vec::new()),
+        Err(err) => {
+            return Err(err)
+                .wrap_err("Could not read config which might exist")
+                .with_note(|| format!("path: {}", path.display()))
+        }
+    };
+
+    let s = String::from_utf8(bytes).wrap_err("Corrupt config, contained non utf8")?;
+    toml::from_str(&s).wrap_err("Could not deserialize to list of devices")
+}
+
+pub(crate) fn write(to_lock: &[Device], custom_path: Option<PathBuf>) -> Result<()> {
+    let data =
+        toml::to_string_pretty(&to_lock).wrap_err("Could not serialize list of devices to toml")?;
+
+    let path = custom_path.unwrap_or_else(setup_default_path);
+    if let Some(dir) = path.parent() {
+        if !dir.is_dir() {
+            return Err(
+                eyre!("Dir does not exist!").with_note(|| format!("dir: {}", dir.display()))
+            );
+        }
+    }
+
+    fs::write(path, data.as_bytes()).wrap_err("Could not write serialized list to file")
+}
