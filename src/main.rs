c89cabdabddc43c79ecc4316e812e9d991a7e0fe
dvdsk <noreply@davidsk.dev>
2024-03-19 13:21:53 +0100

    use evdev sys interface instead of evdev command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![feature(thread_sleep_until)]
+#![feature(iter_intersperse)]
+#![feature(slice_flatten)]
 
 use std::{
-    fs::File,
     sync::{
         atomic::{AtomicBool, Ordering},
         mpsc::{channel, Receiver, RecvTimeoutError},
@@ -10,26 +11,34 @@ use std::{
     thread,
 };
 
-mod check_inputs;
-mod cli;
-mod config;
-mod lock;
-mod notification;
-mod wizard;
-mod watch;
-
+use check_inputs::InputResult;
 use clap::Parser;
 use color_eyre::eyre::Context;
 
+mod check_inputs;
+mod cli;
+mod config;
+mod notification;
+mod watch;
+mod wizard;
+
 use crate::check_inputs::inactivity_watcher;
-use crate::check_inputs::wait_for_any_input;
-use crate::lock::Device;
 use crate::notification::notify_all_users;
 
 fn main() -> color_eyre::Result<()> {
-    let online_devices = watch::devices().unwrap();
-
+    color_eyre::install().expect("Only called once");
     let args = cli::Cli::parse();
+
+    // check after args such that help can run without root
+    if let sudo::RunningAs::User = sudo::check() {
+        panic!(concat!(
+            "must run ",
+            env!("CARGO_CRATE_NAME"),
+            " as root user"
+        ));
+    }
+
+    let (online_devices, new) = watch::devices();
     let (work_duration, break_duration, grace_duration) = match args.command {
         cli::Commands::Run {
             work_duration,
@@ -44,24 +53,12 @@ fn main() -> color_eyre::Result<()> {
 
     let to_block =
         config::read(args.config_path).wrap_err("Could not read devices to block from config")?;
-
-    let device_files = to_block
-        .iter()
-        .map(Device::event_path)
-        .map(File::open)
-        .map(Result::unwrap);
-    let device_files2 = to_block
-        .iter()
-        .map(Device::event_path)
-        .map(File::open)
-        .map(Result::unwrap);
+    let (recv_any_input, recv_any_input2) = check_inputs::watcher(new, to_block.clone())
+        .wrap_err("Could not start watching to be locked devices for activaty")?;
 
     let (break_skip_sender, break_skip_receiver) = channel();
     let (work_start_sender, work_start_receiver) = channel();
     let break_skip_is_sent = Arc::new(AtomicBool::new(false));
-
-    let recv_any_input = wait_for_any_input(device_files);
-    let recv_any_input2 = wait_for_any_input(device_files2);
 
     {
         let break_skip_is_sent = break_skip_is_sent.clone();
@@ -79,13 +76,13 @@ fn main() -> color_eyre::Result<()> {
 
     loop {
         notify_all_users("Waiting for input to start work timer...");
-        block_on_new_input(&recv_any_input);
+        block_on_new_input(&recv_any_input).wrap_err("Could not block till new input")?;
         notify_all_users(&format!("Starting work timer for {break_duration:?}"));
         work_start_sender.send(true).unwrap();
         match break_skip_receiver.recv_timeout(break_duration - grace_duration) {
             Ok(_) => {
                 notify_all_users("No input for breaktime");
-                block_on_new_input(&recv_any_input);
+                block_on_new_input(&recv_any_input).wrap_err("Could not block till new input")?;
                 break_skip_is_sent.store(false, Ordering::Release);
                 continue;
             }
@@ -98,29 +95,35 @@ fn main() -> color_eyre::Result<()> {
 
         let mut locks = Vec::new();
 
-        for device in to_block
-            .iter()
-            .map(Device::name)
-            .filter_map(|name| online_devices.lookup(name))
-        {
-            locks.push(device.clone().lock().unwrap());
+        for device_id in to_block.iter().copied() {
+            locks.push(
+                online_devices
+                    .lock(device_id)
+                    .wrap_err("failed to lock one of the inputs")?,
+            );
         }
 
         notify_all_users(&format!("Starting break timer for {work_duration:?}"));
         thread::sleep(work_duration);
 
         for lock in locks {
-            lock.unlock();
+            lock.unlock()?
         }
     }
 }
 
-fn block_on_new_input(recv_any_input: &Receiver<bool>) {
+fn block_on_new_input(recv_any_input: &Receiver<InputResult>) -> color_eyre::Result<()> {
     loop {
-        if recv_any_input.try_recv().is_err() {
-            break;
-        };
+        match recv_any_input.try_recv() {
+            Err(_) => break,
+            Ok(Err(e)) => return Err(e).wrap_err("Error with device file"),
+            Ok(Ok(_)) => (), // old event, ignore
+        }
     }
 
-    recv_any_input.recv().unwrap();
+    match recv_any_input.recv() {
+        Err(_) => return Ok(()), // device disconnected
+        Ok(Err(e)) => return Err(e).wrap_err("Error with device file"),
+        Ok(Ok(_)) => return Ok(()), // new event! stop blocking
+    }
 }
