5753eb219277a80195f9f2f918f2a8ba2bbee62b
Yara <git@yara.blue>
2024-04-21 12:40:45 +0200

    fixes lock warning notification

--- src/integration.rs
+++ src/integration.rs
@@ -28,7 +28,6 @@ impl DurationUntil for Instant {
 pub struct Status {
     update: mpsc::Sender<State>,
     integrator: Option<JoinHandle<Result<()>>>,
-    lock_warning_notification: Option<Duration>,
 }
 
 fn integrate(
@@ -37,6 +36,7 @@ fn integrate(
     state_notifications: bool,
     idle: Arc<Mutex<Instant>>,
     break_duration: Duration,
+    lock_warning_notification: Option<Duration>,
 ) -> Result<()> {
     let mut timeout = Duration::MAX;
     let mut state = State::Waiting;
@@ -60,11 +60,22 @@ fn integrate(
         let msg = match state {
             State::Waiting => String::from("-"),
             State::Work { next_break } => {
+                if let Some(before_break) = lock_warning_notification {
+                    if next_break.duration_until() < before_break {
+                        let msg = format!("locking in {}", fmt_dur(before_break));
+                        notification::notify_all_users(&msg);
+                    }
+                }
+
                 let idle = idle.lock().unwrap().elapsed();
-                let break_dur = break_duration.saturating_sub(idle);
-                let break_dur = fmt_dur(break_dur);
-                let next_break = fmt_dur(next_break.duration_until());
-                format!("{} break in {}", break_dur, next_break)
+                if idle > Duration::from_secs(30) {
+                    let break_dur = break_duration.saturating_sub(idle);
+                    let break_dur = fmt_dur(break_dur);
+                    format!("idle, reset in {}", break_dur)
+                } else {
+                    let next_break = fmt_dur(next_break.duration_until());
+                    format!("break in {}", next_break)
+                }
             }
             State::Break { next_work } => {
                 format!("unlocks in {}", fmt_dur(next_work.duration_until()))
@@ -96,11 +107,10 @@ impl Status {
 
         let (tx, rx) = mpsc::channel();
         let integrator =
-            thread::spawn(move || integrate(&rx, file_status, notifications, idle, break_duration));
+            thread::spawn(move || integrate(&rx, file_status, notifications, idle, break_duration, lock_warning_notification));
         Ok(Self {
             update: tx,
             integrator: Some(integrator),
-            lock_warning_notification,
         })
     }
 
@@ -124,15 +134,6 @@ impl Status {
 
     pub(crate) fn set_working(&mut self, next_break: Instant) {
         self.send(State::Work { next_break });
-
-        if let Some(before_break) = self.lock_warning_notification {
-            thread::spawn(move || {
-                let msg = format!("locking in {}", fmt_dur(before_break));
-                #[allow(clippy::pedantic)]
-                thread::sleep_until(next_break - before_break);
-                notification::notify_all_users(&msg);
-            });
-        }
     }
 
     pub(crate) fn set_break(&mut self, next_work: Instant) {
