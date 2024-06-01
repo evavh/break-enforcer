321fe77608ca611393f1625800b518ddde59698a
Yara <git@yara.blue>
2024-06-01 12:09:21 +0200

    status command now prints error and reconnects

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@
 #![feature(iter_collect_into)]
 
 use clap::Parser;
-use cli::StatusArgs;
 use color_eyre::eyre::Context;
 use color_eyre::{eyre::eyre, Section};
 use tracing_subscriber::fmt::time::uptime;
@@ -14,6 +13,7 @@ mod check_inputs;
 mod cli;
 mod config;
 mod install;
+mod status;
 mod integration;
 mod run;
 mod tcp_api_config;
@@ -57,38 +57,10 @@ fn main() -> color_eyre::Result<()> {
     match cli.command {
         cli::Commands::Run(args) => run::run(args, cli.config_path),
         cli::Commands::Wizard => wizard::run(cli.config_path).wrap_err("Error running wizard"),
-        cli::Commands::Status(args) => status(args).wrap_err("Could not print status"),
+        cli::Commands::Status(args) => status::run(args).wrap_err("Could not print status"),
         cli::Commands::Install(args) => {
             install::set_up(&args, cli.config_path).wrap_err("Could not install")
         }
         cli::Commands::Remove => install::tear_down().wrap_err("Could not remove"),
-    }
-}
-
-fn format_status(msg: String, use_json: bool) -> String {
-    if use_json {
-        format!("{{\"msg\": \"{msg}\"}}")
-    } else {
-        msg
-    }
-}
-
-fn status(
-    StatusArgs {
-        update_period,
-        use_json,
-    }: StatusArgs,
-) -> color_eyre::Result<()> {
-    let mut api =
-        break_enforcer::Api::new().wrap_err("Error interfacing with break-enforcer instance")?;
-    loop {
-        let msg = api.status().wrap_err("Error requesting status message")?;
-        let output = format_status(msg, use_json);
-        println!("{output}");
-        if let Some(period) = update_period {
-            std::thread::sleep(period);
-        } else {
-            return Ok(());
-        }
     }
 }
---
+++ src/status.rs
@@ -0,0 +1,68 @@
+use crate::cli::StatusArgs;
+use break_enforcer::Api;
+use color_eyre::eyre::WrapErr;
+
+fn format_status(status: Result<String, break_enforcer::Error>, use_json: bool) -> String {
+    match (status, use_json) {
+        (Ok(msg), true) => format!("{{\"msg\": \"{msg}\"}}"),
+        (Ok(msg), false) => msg,
+        (Err(err), true) => format!("{{\"msg\": \"{err}\"}}"),
+        (Err(err), false) => err.to_string(),
+    }
+}
+
+#[derive(Default)]
+enum ReconnectingApi {
+    #[default]
+    Disconnected,
+    Connected(Api),
+}
+
+impl ReconnectingApi {
+    fn new() -> Self {
+        ReconnectingApi::Disconnected
+    }
+
+    fn status(&mut self) -> Result<String, break_enforcer::Error> {
+        let placeholder = ReconnectingApi::default();
+        let owned_self = core::mem::replace(self, placeholder);
+
+        let mut api = match owned_self {
+            ReconnectingApi::Disconnected => break_enforcer::Api::new()?,
+            ReconnectingApi::Connected(api) => api,
+        };
+
+        match api.status() {
+            Ok(status) => {
+                *self = ReconnectingApi::Connected(api);
+                Ok(status)
+            }
+            Err(e) => {
+                *self = ReconnectingApi::Disconnected;
+                Err(e)
+            }
+        }
+    }
+}
+
+pub fn run(
+    StatusArgs {
+        update_period,
+        use_json,
+    }: StatusArgs,
+) -> color_eyre::Result<()> {
+    let mut api = ReconnectingApi::new();
+    let Some(period) = update_period else {
+        let msg = api.status().wrap_err("Error requesting status message")?;
+        let output = format_status(Ok(msg), use_json);
+        println!("{output}");
+        return Ok(());
+    };
+
+    loop {
+        let msg = api.status();
+        let output = format_status(msg, use_json);
+        println!("{output}");
+        std::thread::sleep(period);
+    }
+}
