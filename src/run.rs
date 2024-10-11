0f90cfd4cfd4bb7743ba9edbb4229b25e47169f0
Eva van Houten <opensource@evavh.net>
2024-10-11 11:50:52 +0200

    Add optional long break after cumulative work time
    No idle reset yet for long break, though it does take into
    account idle time and resets when counting work time.

--- src/run.rs
+++ src/run.rs
@@ -1,5 +1,5 @@
 use std::path::PathBuf;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 use color_eyre::eyre::{eyre, Context};
 use color_eyre::{Result, Section};
@@ -15,6 +15,8 @@ pub(crate) fn run(
     RunArgs {
         work_duration,
         break_duration,
+        long_break_duration,
+        work_between_long_breaks,
         lock_warning,
         lock_warning_type,
         status_file,
@@ -23,6 +25,11 @@ pub(crate) fn run(
     }: RunArgs,
     config_path: Option<PathBuf>,
 ) -> Result<()> {
+    let short_break_duration = break_duration;
+    if let Some(long_break_duration) = long_break_duration {
+        assert!(long_break_duration > short_break_duration);
+    }
+
     let (online_devices, new) = watch_and_block::devices();
 
     let to_block = config::read(config_path)
@@ -38,8 +45,9 @@ pub(crate) fn run(
     let (recv_any_input, recv_any_input2) =
         check_inputs::watcher(new, to_block.clone());
 
+    let mut worked_since_long_break = Duration::from_secs(0);
     let mut inactivity_tracker =
-        InactivityTracker::new(recv_any_input2, break_duration);
+        InactivityTracker::new(recv_any_input2, short_break_duration);
     let notify_config = integration::NotifyConfig {
         lock_warning,
         lock_warning_type,
@@ -48,23 +56,36 @@ pub(crate) fn run(
     };
 
     let idle = inactivity_tracker.idle_handle();
-    let mut status =
-        Status::new(status_file, tcp_api, notify_config, idle, break_duration)
-            .wrap_err("Could not setup status reporting")?;
+    let mut status = Status::new(
+        status_file,
+        tcp_api,
+        notify_config,
+        idle,
+        short_break_duration,
+    )
+    .wrap_err("Could not setup status reporting")?;
 
     loop {
         status.set_waiting();
 
         wait_for_user_activity(&recv_any_input)
             .wrap_err("Could not wait for activity")?;
-        status.set_working(Instant::now() + work_duration);
+        let work_start = Instant::now();
+        status.set_working(work_start + work_duration);
 
         let idle = match inactivity_tracker.reset_or_timeout(work_duration) {
             TrackResult::Error(e) => {
                 Err(e).wrap_err("Could not track inactivity")?
             }
-            TrackResult::ShouldReset => continue,
-            TrackResult::ShouldBreak { user_idle } => user_idle,
+            TrackResult::ShouldReset => {
+                worked_since_long_break +=
+                    work_start.elapsed() - short_break_duration;
+                continue;
+            }
+            TrackResult::ShouldBreak { user_idle } => {
+                worked_since_long_break += work_start.elapsed() - user_idle;
+                user_idle
+            }
         };
 
         let mut locks = Vec::new();
@@ -76,8 +97,23 @@ pub(crate) fn run(
             );
         }
 
-        status.set_break(Instant::now() + break_duration - idle);
-        thread::sleep(break_duration - idle);
+        let break_duration = match (long_break_duration, work_between_long_breaks) {
+            (Some(long_break_duration), Some(work_between_long_breaks))
+                // There is always some idle time before the break,
+                // so we add some margin
+                if worked_since_long_break + work_duration / 10
+                    >= work_between_long_breaks =>
+            {
+                worked_since_long_break = Duration::from_secs(0);
+                long_break_duration - idle
+            }
+            _ => {
+                short_break_duration - idle
+            }
+        };
+
+        status.set_break(Instant::now() + break_duration);
+        thread::sleep(break_duration);
 
         for lock in locks {
             lock.unlock()?;
