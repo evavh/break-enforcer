c219fcdd171f4604d79c2da9d0a1be4e1b39d839
Yara <git@yara.blue>
2025-01-06 18:38:25 +0100

    error if runtime dependency required for config and not available

--- src/integration/notification.rs
+++ src/integration/notification.rs
@@ -57,6 +57,36 @@ pub(crate) fn beep() -> Result<()> {
     Ok(())
 }
 
+pub(crate) fn command_available(
+    cmd: &str,
+    expected_output: &str,
+    packages_help: &'static str,
+) -> color_eyre::Result<()> {
+    match Command::new(cmd).arg("--version").output() {
+        Ok(out) => {
+            let stdout = String::from_utf8_lossy(&out.stdout);
+            if stdout.contains(expected_output) {
+                Ok(())
+            } else {
+                Err(eyre!("{cmd} is in path but gave strange output")
+                    .with_note(|| format!("{cmd} output: {stdout}")))
+            }
+        }
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+            Err(eyre!("could not find {cmd} in path")).suggestion(packages_help)
+        }
+        Err(e) => Err(e).wrap_err("Could not investigate whether aplay is installed"),
+    }
+}
+
+pub(crate) fn beep_available() -> color_eyre::Result<()> {
+    command_available(
+        "aplay",
+        "aplay: version",
+        "on most systems aplay is provided by alsa-utils",
+    )
+}
+
 pub(crate) fn notify(text: &str) -> Result<()> {
     for User { id, name } in all_users().wrap_err("Could not get logged in users")? {
         let command = format!("sudo -u {name} DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/{id}/bus notify-send -t 5000 \"{text}\"");
@@ -69,4 +99,12 @@ pub(crate) fn notify(text: &str) -> Result<()> {
     }
 
     Ok(())
+}
+
+pub(crate) fn nofity_available() -> color_eyre::Result<()> {
+    command_available(
+        "notify-send",
+        "notify-send ",
+        "provided by the package libnotify-bin or libnotify",
+    )
 }
