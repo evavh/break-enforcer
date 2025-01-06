d08267888678245a558db7933411662bf3210d8c
Yara <git@yara.blue>
2025-01-06 18:39:03 +0100

    suggest to check if --tcp-api is passed

--- src/status.rs
+++ src/status.rs
@@ -1,6 +1,7 @@
 use crate::cli::StatusArgs;
 use break_enforcer::Api;
 use color_eyre::eyre::WrapErr;
+use color_eyre::Section;
 
 fn format_status(status: Result<String, break_enforcer::Error>, use_json: bool) -> String {
     match (status, use_json) {
@@ -53,7 +54,13 @@ pub fn run(
 ) -> color_eyre::Result<()> {
     let mut api = ReconnectingApi::new();
     let Some(period) = update_period else {
-        let msg = api.status().wrap_err("Error requesting status message")?;
+        let msg = api
+            .status()
+            .wrap_err("Error requesting status message")
+            .suggestion(
+                "Is break-enforcer running and is it running with its tcp api \
+                enabled? (use --tcp-api)",
+            )?;
         let output = format_status(Ok(msg), use_json);
         println!("{output}");
         return Ok(());
