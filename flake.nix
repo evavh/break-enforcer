3124f740dc8efe675f4368dc6d7d491c7b15aff3
Yara <git@yara.blue>
2025-12-07 15:20:05 +0100

    adds flake, missing assets folder though

---
+++ flake.nix
@@ -0,0 +1,149 @@
+{
+  description = "Software break enforcer, with activity detection.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs { inherit system overlays; };
+
+          ####################################################################
+          #### break-enforcer package                                     ####
+          ####################################################################
+          break-enforcer = with pkgs; let
+            inherit (nix-gitignore) gitignoreFilterPure withGitignoreFile;
+			# Workaround for the builtins.filterSource issue mentioned in
+			# https://nixos.org/manual/nix/unstable/expressions/builtins.html
+			# Since this might be built from a flake, the source path may be a
+			# store path, so we need to provide our own version of
+			# gitignoreSource that avoids builtins.filterSource in favor of
+			# builtins.path.
+            gitignoreSource = patterns: path:
+              builtins.path {
+                filter =
+                  gitignoreFilterPure (_: _: true) (withGitignoreFile patterns path) path;
+                path = path;
+                name = "src";
+              };
+
+			# Ignore some extra things that don't factor into the main build to
+			# help with caching.
+            extraIgnores = ''
+              /.envrc
+              /*.nix
+              /flake.*
+              /netlify.toml
+              /.github
+              /assets
+              /*.md
+              /.gitignore
+              /LICENSE
+            '';
+
+            src = gitignoreSource extraIgnores ./.;
+
+            cargoTOML = lib.importTOML "${src}/Cargo.toml";
+            rustToolchain = rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+            rust = makeRustPlatform {
+              cargo = rustToolchain;
+              rustc = rustToolchain;
+            };
+          in
+          rust.buildRustPackage
+            {
+              pname = cargoTOML.package.name;
+              version = cargoTOML.package.version;
+
+              inherit src;
+
+              cargoLock = { lockFile = "${src}/Cargo.lock"; };
+
+              meta = {
+                inherit (cargoTOML.package) description homepage;
+                maintainers = cargoTOML.package.authors;
+              };
+            };
+
+          ####################################################################
+          #### dev shell                                                  ####
+          ####################################################################
+          devShell = with pkgs;
+            mkShell {
+              name = "break-enforcer";
+              inputsFrom = [ break-enforcer ];
+              RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+              CARGO_TERM_COLOR = "always";
+            };
+        in
+        {
+          apps = {
+            break-enforcer = {
+              type = "app";
+              program = "${break-enforcer}/bin/break-enforcer";
+              description = "Software break enforcer, with activity detection.";
+            };
+            default = self.apps.${system}.break-enforcer;
+          };
+          devShells.default = devShell;
+          packages = {
+            inherit break-enforcer;
+            default = self.packages.${system}.break-enforcer;
+          };
+          checks = {
+            inherit break-enforcer;
+          };
+        });
+}
+
+# {
+#   lib,
+#   rustPlatform,
+#   fetchFromGitHub,
+#   stdenv,
+#   darwin,
+#   pkgs,
+# }:
+#
+# # let 
+# # rustPlatform = pkgs.makeRustPlatform {
+# # 	cargo = pkgs.rust-bin.stable.lastest.default;
+# # 	rustVersion = pkgs.rust-bin.stable.lastest.default;
+# # };
+# # in {
+# rustPlatform.buildRustPackage rec {
+#   pname = "break-enforcer";
+#   version = "0.3.2";
+#
+#   src = fetchFromGitHub {
+#     owner = "evavh";
+#     repo = "break-enforcer";
+#     rev = version;
+#     hash = "sha256-I1tr37DQyXFB4ucutQv84tbK8VtuF1kVXSb7ayyfkGY=";
+#   };
+#
+#   cargoHash = "sha256-GAC9sKiGyaTY2LnGOxVGTxXteAVOeQZZ79N4ae2GOrY=";
+#
+#   packages = with pkgs; [
+#   	alsa-utils
+#   ];
+#
+#   meta = {
+#     description = "Software break enforcer, with activity detection";
+#     homepage = "https://github.com/evavh/break-enforcer";
+#     changelog = "https://github.com/evavh/break-enforcer/blob/${src.rev}/CHANGELOG.md";
+#     maintainers = with lib.maintainers; [ ];
+#     mainProgram = "break-enforcer";
+#   };
+# }
