653ad94461b28b554f4c5b579a4ab222b54616aa
Yara <git@yara.blue>
2026-02-06 17:09:27 +0100

    add all options to nixmodule

--- nix_module.nix
+++ nix_module.nix
@@ -1,6 +1,7 @@
 {config, lib, pkgs, ...}:
 
 with lib;
+with lib.types;
 let
 	cfg = config.services.break-enforcer;
 in
@@ -8,6 +9,76 @@ in
 	options = {
 		services.break-enforcer = {
 			enable = mkEnableOption "break-enforcer";
+			work-duration = mkOption {
+				type = types.str;
+				description = "Period after which input will be disabled. Note:
+				run help command to see the duration format";
+			};
+			break-duration = mkOption {
+				type = types.str;
+				description = "Length of the (short) breaks, after this period
+				input is resumed. Note: run help command to see the duration
+				format";
+			};
+			long-break-duration = mkOption {
+				type = types.nullOr types.str;
+				default = null;
+				description = "Length of the long breaks, after this period
+				input is resumed. Note: run help command to see the duration
+				format";
+			};
+			work-between-long-breaks = mkOption {
+				type = types.nullOr types.str;
+				default = null;
+				description = "Amount of total work time before next break will
+				be a long break. Note: run help command to see the duration
+				format";
+			};
+			break-start-lead = mkOption {
+				type = types.str;
+				description = "How long before a break starts to send a
+				notification. Note: run help command to see the duration
+				format";
+				default = "30s";
+			};
+			break-end-lead = mkOption {
+				type = types.str;
+				description = "How long before a break starts to send a
+				notification. Note: run help command to see the duration
+				format";
+				default = "5s";
+			};
+			work-reset-lead = mkOption {
+				type = types.str;
+				description = "How long before the work period resets to send a
+				notification. Note: run help command to see the duration
+				format";
+				default = "5s";
+			};
+			break-start-notify = mkOption {
+				type = listOf (enum ["system" "audio"]);
+				default = [];
+				description = "Type of notification to get when break is about
+				to begin. - For audio you need aplay installed. - For system you
+				need notify-send installed [possible values: system, audio]";
+			};
+			break-end-notify = mkOption {
+				type = listOf (enum ["system" "audio"]);
+				default = [];
+				description = "Type of notification to get when break is about
+				to end. - For audio you need aplay installed. - For system you
+				need notify-send installed [possible values: system, audio]";
+			};
+			work-reset-notify = mkOption {
+				type = listOf (enum ["system" "audio"]);
+				default = [];
+				description = "Type of notification to get when break is about
+				to end. - For audio you need aplay installed. - For system you
+				need notify-send installed [possible values: system, audio]";
+			};
+			tcp-api = mkEnableOption "tcp-api";
+			status-file = mkEnableOption "status-file";
+			notifications = mkEnableOption "notifications";
 		};
 	};
 
@@ -21,26 +92,26 @@ in
 				Type = "simple";
 				ExecStart = ''
 					${pkgs.break-enforcer}/bin/break-enforcer run \
-					--work-duration 25:00 \
-					--break-duration 05:00 \
-					--break-start-lead 30s \
-					--break-end-lead 5s \
-					--break-start-notify audio \
-					--break-end-notify audio \
-					--tcp-api
+					--work-duration ${cfg.work-duration} \
+					--break-duration ${cfg.break-duration} \
+					 ${optionalString (cfg.long-break-duration != null)
+					 "--long-break-duration ${cfg.long-break-duration}"}
+					 ${optionalString (cfg.work-between-long-breaks != null)
+					 "--work-between-long-breaks ${cfg.work-between-long-breaks}"}
+					--break-start-lead ${cfg.break-start-lead} \
+					--break-end-lead ${cfg.break-end-lead} \
+					--work-reset-lead ${cfg.work-reset-lead} \
+					${concatMapStrings (x: "--break-start-notify " + x)
+					cfg.break-start-notify} \
+					${concatMapStrings (x: "--break-end-notify " + x)
+					cfg.break-end-notify} \
+					${concatMapStrings (x: "--work-reset-notify " + x)
+					cfg.work-reset-notify} \
+					${optionalString cfg.tcp-api "--tcp-api"} \
+					${optionalString cfg.status-file "--status-file"} \
+					${optionalString cfg.notifications "--notifications"}
 					'';
 			};
 		};
 	};
 }
-
-# {
-# 	# options = {
-# 	# 	enabled = mkOption {
-# 	# 		type = types.bool;
-# 	# 		default = true;
-# 	# 		description = "a test";
-# 	# 	}
-# 	# }
-#
-# }
